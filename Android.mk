deecc2771106952053ff0120d5e07a0afabf5d0e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2010-12-29 14:41:32 +0000

    Initial version of the GPS shim
    This should work with most versions of libgps or libloc_api that use
    the old ABI, and wrap them into the new HAL syntax

---
+++ Android.mk
@@ -0,0 +1,35 @@
+ifneq ($(BOARD_GPS_LIBRARIES),)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := gps.$(TARGET_BOOTLOADER_BOARD_NAME)
+
+LOCAL_SHARED_LIBRARIES:= \
+	liblog \
+    $(BOARD_GPS_LIBRARIES) \
+
+LOCAL_SRC_FILES += \
+    gps.c
+
+LOCAL_CFLAGS += \
+    -fno-short-enums 
+
+ifneq ($(BOARD_GPS_BAD_AGPS),)
+LOCAL_CFLAGS += \
+    -DNO_AGPS
+endif
+
+#ifneq ($(BOARD_GPS_NEEDS_XTRA),)
+LOCAL_CFLAGS += \
+    -DNEEDS_INITIAL_XTRA
+#endif
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
